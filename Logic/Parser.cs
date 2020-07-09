e0d22c90fb02f0e3023c16efcab46c2003b512e1
Cheymos <nevesomov@gmail.com>
2020-07-09 14:42:54 +0600

    Init some-some

---
+++ Logic/Parser.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ClosedXML.Excel;
+using FSTEC_Analytics.Models;
+using Xceed.Words.NET;
+
+namespace FSTEC_Analytics.Logic
+{
+
+    internal class Parser
+    {
+        public event Action<bool> IsDoneSuccess;
+
+        private string fileName;
+        private DateTime dateFrom;
+        private DateTime dateTo;
+        private bool buttonKselected;
+        private string outputFileName;
+
+        private VulnerabilityModel vulnerability = new VulnerabilityModel();
+
+        public Parser(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public async void Start(DateTime dateFrom, DateTime dateTo, bool buttonKselected, string outputFileName)
+        {
+            this.dateFrom = dateFrom;
+            this.dateTo = dateTo;
+            this.buttonKselected = buttonKselected;
+            this.outputFileName = outputFileName;
+
+            await Task.Run(() => ParseExcel());
+        }
+
+        private void ParseExcel()
+        {
+            try
+            {
+                using (var workbook = new XLWorkbook(fileName, XLEventTracking.Disabled))
+                {
+                    var worksheet = workbook.Worksheet(1);
+
+                    var c = 0; // Количество элементов подходящие под гугл хром
+
+                    foreach (var row in worksheet.RowsUsed().Skip(2))
+                    {
+                        if (row.Cell(5).Value.ToString() == "Google Chrome" /*&& (DateTime)row.Cell(10).Value <= DateTime*/)
+                        {
+                            c++;
+
+                            VulnerabilityObject item = new VulnerabilityObject
+                            {
+                                ID = row.Cell(1).Value.ToString(),
+                                Title = row.Cell(2).Value.ToString(),
+                                Description = row.Cell(3).Value.ToString(),
+                                VendorPO = row.Cell(4).Value.ToString(),
+                                NamePO = row.Cell(5).Value.ToString(),
+                                VersionPO = row.Cell(6).Value.ToString(),
+                                TypePO = row.Cell(7).Value.ToString(),
+                                OS = row.Cell(8).Value.ToString(),
+                                Class = row.Cell(9).Value.ToString(),
+                                Date = (DateTime)row.Cell(10).Value,
+                                CVSS2 = row.Cell(11).Value.ToString(),
+                                CVSS3 = row.Cell(12).Value.ToString(),
+                                DangerLevel = row.Cell(13).Value.ToString(),
+                                PossibleCorrective = row.Cell(14).Value.ToString(),
+                                Status = row.Cell(15).Value.ToString(),
+                                Exploit = row.Cell(16).Value.ToString(),
+                                Information = row.Cell(17).Value.ToString(),
+                                URL = row.Cell(18).Value.ToString(),
+                                OtherIdentifiers = row.Cell(19).Value.ToString(),
+                                Etc = row.Cell(20).Value.ToString()
+                            };
+
+                            vulnerability.AddItem(item);
+                        }
+                    }
+
+                    if (buttonKselected)
+                    {
+                        PutOnFile();
+                    }
+                    else
+                    {
+                        PutOnFileFull();
+                    }
+                }
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Программа поддерживает анализ файлов только данных банка уязвимостей ФСТЭК в формате\".xlsx\"\nПожалуйста, повторите попытку!", "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Error);
+                IsDoneSuccess?.Invoke(false);
+            }
+            catch (InvalidCastException)
+            {
+                MessageBox.Show("Программа поддерживает анализ файлов только данных банка уязвимостей ФСТЭК в формате\".xlsx\"\nПожалуйста, повторите попытку!", "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Error);
+                IsDoneSuccess?.Invoke(false);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Error);
+                IsDoneSuccess?.Invoke(false);
+            }
+        }
+
+        private void PutOnFileFull()
+        {
+            using (var document = DocX.Create(outputFileName))
+            {
+                try
+                {
+                    var paragrahp = document.InsertParagraph();
+
+                    VulnerabilityModel filter = vulnerability.Report(dateFrom, dateTo);
+
+                    for (int i = 0; i < filter.Count; i++)
+                    {
+                        var item = filter.GetItem(i);
+                        paragrahp.Append($"{item.Date:D} | {item.ID} | {item.Title} | {item.NamePO}\n\n");
+                    }
+
+                    document.Save();
+                    IsDoneSuccess?.Invoke(true);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Error);
+                    IsDoneSuccess?.Invoke(false);
+                }
+            }
+        }
+
+        private void PutOnFile()
+        {
+            using (var document = DocX.Create(outputFileName))
+            {
+                try
+                {
+                    var paragrahp = document.InsertParagraph();
+
+                    VulnerabilityModel filter = vulnerability.Report(dateFrom, dateTo);
+
+                    paragrahp.Append("Распределение количества уязвимостей по годам:\n");
+                    foreach (var item in vulnerability.ObjectListYear)
+                    {
+                        paragrahp.Append($"{item.Year} г. - {item.Count}\n");
+                    }
+                    paragrahp.Append("\n\n\n");
+
+                    paragrahp.Append($"Уровни опасности за {dateFrom:D} по {dateTo:D}\n"); ;
+                    paragrahp.Append($"Критический уровень: {vulnerability.Critical} уязвимостей\n" +
+                                     $"Высокий уровень: {vulnerability.High} уязвимостей\n" +
+                                     $"Средний уровень: {vulnerability.Middle} уязвимостей\n" +
+                                     $"Высокий уровень: {vulnerability.Low} уязвимостей");
+
+                    document.Save();
+                    IsDoneSuccess?.Invoke(true);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Input;
+using FSTEC_Analytics.Logic;
+using Microsoft.Win32;
+
+namespace FSTEC_Analytics
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string fileName;
+        bool buttonKselected = false;
+        string outputFileName = "report_FSTEC.docx";
+        string dowloadLink = @"https://bdu.fstec.ru/files/documents/vullist.xlsx";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GridFieldDrop_DragEnter(object sender, DragEventArgs e)
+        {
+            DragText.Text = "Можете отпускать!";
+        }
+
+        private void GridFieldDrop_DragLeave(object sender, DragEventArgs e)
+        {
+            DragText.Text = "Сюда можно бросить файл!";
+
+        }
+
+        private void GridFieldDrop_Drop(object sender, DragEventArgs e)
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            fileName = files[0].ToString();
+
+            DragText.Text = fileName;
+        }
+
+        private void GridFieldDrop_MouseDown(object sender, MouseButtonEventArgs e) // Вызов OpenFileDialog
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Excel Worksheets|*.xlsx",
+                Title = "Выберите таблицу Excel"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                if (openFileDialog.CheckFileExists)
+                {
+                    fileName = openFileDialog.FileName;
+                    DragText.Text = fileName;
+                }
+            }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (DateTime.TryParse(DateFrom.Text, out DateTime dateFrom) && DateTime.TryParse(DateTo.Text, out DateTime dateTo))
+            {
+                if (fileName != null)
+                {
+                    if (File.Exists(fileName))
+                    {
+                        var result = MessageBox.Show($"Готовый отчёт уже существует и будет перезаписан\nПродолжить выполнение?", "Информация", MessageBoxButton.OKCancel, MessageBoxImage.Asterisk);
+                        if (result == MessageBoxResult.OK)
+                        {
+                            var parser = new Parser(fileName);
+
+                            parser.Start(dateFrom, dateTo, buttonKselected, outputFileName);
+                            parser.IsDoneSuccess += Parser_IsDoneSuccess;
+
+                            Window.IsEnabled = false;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Файл не выбран.\nВыберите файл и повторите попытку!", "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Некорректно введена дата!", "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Parser_IsDoneSuccess(bool obj)
+        {
+            Dispatcher.Invoke(new Action(delegate () { Window.IsEnabled = true; DragText.Text = "Сюда можно бросить файл!"; }));
+            if (obj == true)
+                MessageBox.Show($"Отчёт [{outputFileName}] успешно сформирован в директории рядом с программой", "Информация", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+        }
+
+        private void Button_P_Click(object sender, RoutedEventArgs e)
+        {
+            buttonKselected = false;
+
+            if (Button_P.IsEnabled)
+            {
+                Button_P.IsEnabled = false;
+                Button_K.IsEnabled = true;
+            }
+        }
+
+        private void Button_K_Click(object sender, RoutedEventArgs e)
+        {
+            buttonKselected = true;
+
+            if (Button_K.IsEnabled)
+            {
+                Button_K.IsEnabled = false;
+                Button_P.IsEnabled = true;
+            }
+        }
+
+        private void DownloadFile()
+        {
+            fileName = "FSTEC_file.xlsx";
+            if (File.Exists($"{fileName}"))
+            {
+                var result = MessageBox.Show($"Файл данных банка уязвимостей ФСТЭК уже был загружен раннее\nСкачать заново?", "Информация", MessageBoxButton.YesNo, MessageBoxImage.Asterisk);
+
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    File.Delete($"{fileName}");
+                }
+                else if (result == MessageBoxResult.No)
+                {
+                    fileName = $"{fileName}";
+                    DragText.Text = fileName;
+                    return;
+                }
+            }
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileAsync(new Uri(dowloadLink), ($"{fileName}"));
+            webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+            Window.IsEnabled = false;
+        }
+
+        private void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show($"Файл загружен!", "Информация", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+
+            Parser_IsDoneSuccess(false);
+            DragText.Text = fileName;
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DownloadFile();
+        }
+    }
+}
